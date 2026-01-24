64b7c342a4f72da11ebcd2069ea4e0d63ca88ea9
filip-andrei19 <rotar.c.filipandrei24@stud.rau.ro>
2026-01-24 15:09:17 +0200

    qdwd

--- Bk/server.js
+++ Bk/server.js
@@ -157,7 +157,7 @@ const startServer = async () => {
 
         // --- 3. CRON JOB INTELIGENT ---
         // Rulează la ora 15:03 (Minutul 3, Ora 15)
-        cron.schedule('3 15 * * *', async () => {
+        cron.schedule('12 15 * * *', async () => {
             console.log('⏰ [CRON] Pornesc actualizarea zilnică...');
             await runDailyJob(); 
         });
